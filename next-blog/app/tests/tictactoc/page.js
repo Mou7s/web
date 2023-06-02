65e73fa045a3dfa9682502d9b19c114c55e93e69
Mou7s <liuchuge7777777@gmail.com>
2023-06-02 20:40:34 +0800

    #

--- next-blog/app/tests/tictactoc/page.js
+++ next-blog/app/tests/tictactoc/page.js
@@ -4,19 +4,51 @@ import styles from "../../page.module.css";
 import ticStyles from "../../tictactoc.module.css";
 import Link from "next/link";
 import dynamic from "next/dynamic";
-import { Board, Square } from "./app.js";
 
-const DynamicComponent = dynamic(() => import("./app.js"));
+function Square() {
+  const [value, setValue] = useState();
+
+  function handleClick() {
+    setValue("X");
+  }
+
+  return (
+    <button className={ticStyles.square} onClick={handleClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  return (
+    <div>
+      <div className={ticStyles.boardRow}>
+        <Square value={squares[0]}></Square>
+        <Square value={squares[1]}></Square>
+        <Square value={squares[2]}></Square>
+      </div>
+      <div className={ticStyles.boardRow}>
+        <Square value={squares[3]}></Square>
+        <Square value={squares[4]}></Square>
+        <Square value={squares[5]}></Square>
+      </div>
+      <div className={ticStyles.boardRow}>
+        <Square value={squares[6]}></Square>
+        <Square value={squares[7]}></Square>
+        <Square value={squares[8]}></Square>
+      </div>
+    </div>
+  );
+}
 
 export default function Pages() {
   return (
-    <main className={styles.main}>
+    <div className={styles.main}>
       <div className={styles.center}>
-        <h1>Tic-Tac-Toe</h1>
+        <h1>Tictactoc</h1>
       </div>
-      <div className={styles.center}>
-        <DynamicComponent></DynamicComponent>
-      </div>
-    </main>
+      <Board></Board>
+    </div>
   );
 }
