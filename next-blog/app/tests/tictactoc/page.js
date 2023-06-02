5573aac12c98c09be6d5d8277399b30a70b2bfe5
Mou7s <liuchuge7777777@gmail.com>
2023-06-02 22:25:09 +0800

    Update page.js

--- next-blog/app/tests/tictactoc/page.js
+++ next-blog/app/tests/tictactoc/page.js
@@ -4,15 +4,9 @@ import styles from "../../page.module.css";
 import ticStyles from "../../tictactoc.module.css";
 import Link from "next/link";
 
-function Square() {
-  const [value, setValue] = useState();
-
-  function handleClick() {
-    setValue("X");
-  }
-
+function Square({ value, onSquareClick }) {
   return (
-    <button className={ticStyles.square} onClick={handleClick}>
+    <button className={ticStyles.square} onClick={onSquareClick}>
       {value}
     </button>
   );
@@ -20,22 +14,56 @@ function Square() {
 
 function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <div>
       <div className={ticStyles.boardRow}>
-        <Square value={squares[0]}></Square>
-        <Square value={squares[1]}></Square>
-        <Square value={squares[2]}></Square>
+        <Square
+          value={squares[0]}
+          onSquareClick={() => handleClick(0)}
+        ></Square>
+        <Square
+          value={squares[1]}
+          onSquareClick={() => handleClick(1)}
+        ></Square>
+        <Square
+          value={squares[2]}
+          onSquareClick={() => handleClick(2)}
+        ></Square>
       </div>
       <div className={ticStyles.boardRow}>
-        <Square value={squares[3]}></Square>
-        <Square value={squares[4]}></Square>
-        <Square value={squares[5]}></Square>
+        <Square
+          value={squares[3]}
+          onSquareClick={() => handleClick(3)}
+        ></Square>
+        <Square
+          value={squares[4]}
+          onSquareClick={() => handleClick(4)}
+        ></Square>
+        <Square
+          value={squares[5]}
+          onSquareClick={() => handleClick(5)}
+        ></Square>
       </div>
       <div className={ticStyles.boardRow}>
-        <Square value={squares[6]}></Square>
-        <Square value={squares[7]}></Square>
-        <Square value={squares[8]}></Square>
+        <Square
+          value={squares[6]}
+          onSquareClick={() => handleClick(6)}
+        ></Square>
+        <Square
+          value={squares[7]}
+          onSquareClick={() => handleClick(7)}
+        ></Square>
+        <Square
+          value={squares[8]}
+          onSquareClick={() => handleClick(8)}
+        ></Square>
       </div>
     </div>
   );
