e9c22caa89f822dda22a4512113f3a2a23022926
Mou7s <liuchuge7777777@gmail.com>
2023-06-02 23:13:35 +0800

    #

--- next-blog/app/tests/tictactoc/page.js
+++ next-blog/app/tests/tictactoc/page.js
@@ -17,13 +17,31 @@ function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
   return (
     <div>
+      <div className={styles.status}>{status}</div>
+
       <div className={ticStyles.boardRow}>
         <Square
           value={squares[0]}
@@ -70,17 +88,34 @@ function Board() {
   );
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 export default function Pages() {
   return (
     <div className={styles.main}>
       <div className={styles.center}>
         <h1>Tictactoc</h1>
       </div>
-      <div className={styles.center}>
-      <Board></Board>
-
+      <div className={styles.main}>
+        <Board></Board>
       </div>
-
     </div>
   );
 }
