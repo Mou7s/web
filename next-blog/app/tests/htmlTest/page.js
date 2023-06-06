e3b4a1bd6f2973d0c51672bfb31763dfbf5ef570
mou7s <liuchuge7777777@gmail.com>
2023-06-06 14:42:50 +0800

    #

--- next-blog/app/tests/htmlTest/page.js
+++ next-blog/app/tests/htmlTest/page.js
@@ -5,12 +5,23 @@ import styles from "../../page.module.css";
 import Link from "next/link";
 
 function Gallery() {
-  let [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(0);
   const [showMore, setShowMore] = useState(false);
 
-  function handleNextClick() {
-    setIndex(index + 1);
+  let hasPrev = index > 0;
+  let hasNext = index < sculptureList.length - 1;
+  
+  function handlePrevClick() {
+    if (hasPrev) {
+      setIndex(index - 1);
+    }
   }
+  function handleNextClick() {
+    if (hasNext) {
+      setIndex(index + 1);
+    }
+  }
+
   function handleMoreClick() {
     setShowMore(!showMore);
   }
@@ -18,7 +29,24 @@ function Gallery() {
   let sculpture = sculptureList[index];
   return (
     <div>
-      <button></button>
+      <button onClick={handlePrevClick} disabled={!hasPrev}>
+        Previous
+      </button>
+      <button onClick={handleNextClick} disabled={!hasNext}>
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name} </i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? "Hide" : "Show"} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img src={sculpture.url} alt={sculpture.alt} />
     </div>
   );
 }
