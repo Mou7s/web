e62288dceeaae055a7357acc173c465300dd4133
Mou7s <liuchuge7777777@gmail.com>
2023-06-08 17:26:25 +0800

    #

--- next-blog/app/tests/state/page.js
+++ next-blog/app/tests/state/page.js
@@ -1,65 +1,21 @@
 "use client";
 import React, { useState, StrictMode, useEffect } from "react";
-import { sculptureList } from "./data";
 import styles from "../../page.module.css";
-import Link from "next/link";
 
-function Gallery() {
-  const [index, setIndex] = useState(0);
-  const [showMore, setShowMore] = useState(true);
-
-  let hasPrev = index > 0;
-  let hasNext = index < sculptureList.length - 1;
-  
-  function handlePrevClick() {
-    if (hasPrev) {
-      setIndex(index - 1);
-    }
-  }
-  function handleNextClick() {
-    if (hasNext) {
-      setIndex(index + 1);
-    }
-  }
-
-  function handleMoreClick() {
-    setShowMore(!showMore);
-  }
-
-  let sculpture = sculptureList[index];
-  return (
-    <div>
-      <button onClick={handlePrevClick} disabled={!hasPrev}>
-        Previous
-      </button>
-      <button onClick={handleNextClick} disabled={!hasNext}>
-        Next
-      </button>
-      <h2>
-        <i>{sculpture.name} </i>
-        by {sculpture.artist}
-      </h2>
-      <h3>
-        ({index + 1} of {sculptureList.length})
-      </h3>
-      <button onClick={handleMoreClick}>
-        {showMore ? "Hide" : "Show"} details
-      </button>
-      {showMore && <p>{sculpture.description}</p>}
-      <img src={sculpture.url} alt={sculpture.alt} />
-    </div>
-  );
+function AlertButton({ message, children }) {
+  return <button onClick={() => alert(message)}>{children}</button>;
 }
 
 export default function Pages() {
   return (
     <div className={styles.main}>
-      <div className={styles.center}>
-        <h1>html test</h1>
-      </div>
+      <div className={styles.center}>title</div>
 
-      <div>
-        <Gallery></Gallery>
+      <div className={styles.main}>
+        <div>
+          <AlertButton message="正在播放！">播放电影</AlertButton>
+          <AlertButton message="正在上传！">上传图片</AlertButton>
+        </div>
       </div>
     </div>
   );
