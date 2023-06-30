4916e37db1791a061c703b0b9274028000847534
Mou7s <liuchuge7777777@gmail.com>
2023-07-01 00:18:29 +0800

    #

---
+++ algorithm/bubbleSort.js
@@ -0,0 +1,16 @@
+var bubbleSort = function (arr) {
+  var len = arr.length;
+
+  for (var i = 0; i < len - 1; i++) {
+    for (var j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // 交换相邻元素的位置
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+};
--- algorithm/quicksort.js
+++ algorithm/quicksort.js
@@ -1,6 +1,7 @@
 var quickSort = function (arr) {
-
-  if (arr.length <= 1) { return arr; }
+  if (arr.length <= 1) {
+    return arr;
+  }
 
   var pivotIndex = Math.floor(arr.length / 2);
 
@@ -11,19 +12,12 @@ var quickSort = function (arr) {
   var right = [];
 
   for (var i = 0; i < arr.length; i++) {
-
     if (arr[i] < pivot) {
-
       left.push(arr[i]);
-
     } else {
-
       right.push(arr[i]);
-
     }
-
   }
 
   return quickSort(left).concat([pivot], quickSort(right));
-
 };
