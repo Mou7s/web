6798ce51346ea48674b51f34337e67f5f1a99839
Mou7s <liuchuge7777777@gmail.com>
2023-07-01 15:51:09 +0800

    ngonchange

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  Input,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent implements OnChanges, OnInit {
+  @Input() item!: string;
+  constructor() {
+    console.log('ArticleBodyComponent: constructor');
+  }
+
+  ngOnInit() {
+    console.log('ArticleBodyComponent: ngOnInit');
+  }
+
+  ngOnChanges() {
+    console.log('ArticleBodyComponent: ngOnChanges');
+  }
 }
