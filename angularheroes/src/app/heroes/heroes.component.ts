2bf0dec5733c1336ada3b945257ffa31940109e4
Mou7s <liuchuge7777777@gmail.com>
2023-07-02 03:23:23 +0800

    #

--- angularheroes/src/app/heroes/heroes.component.ts
+++ angularheroes/src/app/heroes/heroes.component.ts
@@ -5,8 +5,15 @@ import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
+  styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent {
+
   heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
