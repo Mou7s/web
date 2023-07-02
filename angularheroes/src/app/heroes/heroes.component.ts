eaf4c74c77a94479095361748d3669003b72d739
Mou7s <liuchuge7777777@gmail.com>
2023-07-03 02:26:41 +0800

    #

--- angularheroes/src/app/heroes/heroes.component.ts
+++ angularheroes/src/app/heroes/heroes.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
-
-export class HeroesComponent {
-
-  heroes = HEROES;
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
+  constructor(private heroService: HeroService) {}
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
