e58eeace635712d6b3a34974a7e0f26f04042259
Mou7s <liuchuge7777777@gmail.com>
2023-07-03 20:50:15 +0800

    #

--- angularheroes/src/app/hero.service.ts
+++ angularheroes/src/app/hero.service.ts
@@ -9,7 +9,7 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
   constructor(private messageService: MessageService) {}
-  
+
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
