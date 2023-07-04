b6adb0640186421e4a8436ffbd3eacfb3441c093
Mou7s <liuchuge7777777@gmail.com>
2023-07-04 16:25:59 +0800

    #

--- angularheroes/src/app/hero-detail/hero-detail.component.ts
+++ angularheroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -7,5 +11,11 @@ import { Hero } from '../hero';
   styleUrls: ['./hero-detail.component.css'],
 })
 export class HeroDetailComponent {
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService, //从远端服务器获取英雄数据，本组件将使用它来获取要显示的英雄。
+    private location: Location
+  ) {}
+
   @Input() hero?: Hero;
 }
