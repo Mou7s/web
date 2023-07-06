fe78054e2b2bab77a723f304d9f6c83654bad3b6
mou7s <liuchuge7777777@gmail.com>
2023-07-07 05:26:32 +0800

    #

--- HTML/test.js
+++ HTML/test.js
@@ -15,7 +15,8 @@ class Professor extends Person {
   }
 
   grade(paper) {
-    // 评分逻辑
+    const score = Math.random() * 100; // 生成一个随机分数
+    console.log(`给${paper}评分：${score}`);
   }
 
   introduceSelf() {
@@ -30,13 +31,21 @@ class Student extends Person {
   }
 
   introduceSelf() {
-    console.log(`我是${this.name}，我是${this.year}年级的学生。`);
+    console.log(
+      `Hello, my name is ${this.name} and I am in year ${this.year}.`
+    );
+  }
+
+  canStudyArchery() {
+    return this.year > 1;
   }
 }
 
 // 示例用法
-walsh = new Professor("沃尔什", "心理学");
-walsh.introduceSelf(); // '我是沃尔什，我是你们的心理学老师。'
+const professor = new Professor("张老师", "数学");
+professor.introduceSelf(); // 输出："我是张老师，我是你们的数学老师。"
+professor.grade("论文"); // 调用评分逻辑
 
-summers = new Student("萨摩斯", 1);
-summers.introduceSelf(); // '我是萨摩斯，我是一年级的学生。'
+const student = new Student("小明", 2);
+student.introduceSelf(); // 输出："Hello, my name is 小明 and I am in year 2."
+console.log(student.canStudyArchery()); // 输出：true
