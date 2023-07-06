4e0540ed06c1b3d1f082c442cc8fb2ea2784cbec
Mou7s <liuchuge7777777@gmail.com>
2023-07-07 03:49:28 +0800

    #

--- HTML/test.js
+++ HTML/test.js
@@ -1,7 +1,42 @@
-const myObject = {
-  city: "Madrid",
-  greet() {
-    console.log(`来自 ${this.city} 的问候`);
-  },
-};
-myObject.greet(); // 来自 Madrid 的问候
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  introduceSelf() {
+    console.log(`我是${this.name}。`);
+  }
+}
+
+class Professor extends Person {
+  constructor(name, teaches) {
+    super(name);
+    this.teaches = teaches;
+  }
+
+  grade(paper) {
+    // 评分逻辑
+  }
+
+  introduceSelf() {
+    console.log(`我是${this.name}，我是你们的${this.teaches}老师。`);
+  }
+}
+
+class Student extends Person {
+  constructor(name, year) {
+    super(name);
+    this.year = year;
+  }
+
+  introduceSelf() {
+    console.log(`我是${this.name}，我是${this.year}年级的学生。`);
+  }
+}
+
+// 示例用法
+walsh = new Professor("沃尔什", "心理学");
+walsh.introduceSelf(); // '我是沃尔什，我是你们的心理学老师。'
+
+summers = new Student("萨摩斯", 1);
+summers.introduceSelf(); // '我是萨摩斯，我是一年级的学生。'
