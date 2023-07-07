57d7909a502732a1d7ab5f3657386264a0dc7a98
Mou7s <liuchuge7777777@gmail.com>
2023-07-07 18:01:20 +0800

    3

--- HTML/test.js
+++ HTML/test.js
@@ -1,51 +1,28 @@
-class Person {
-  constructor(name) {
+class Shape {
+  constructor(name, sides, sideLength) {
     this.name = name;
+    this.sides = sides;
+    this.sideLength = sideLength;
   }
 
-  introduceSelf() {
-    console.log(`我是${this.name}。`);
-  }
-}
-
-class Professor extends Person {
-  constructor(name, teaches) {
-    super(name);
-    this.teaches = teaches;
-  }
-
-  grade(paper) {
-    const score = Math.random() * 100; // 生成一个随机分数
-    console.log(`给${paper}评分：${score}`);
-  }
-
-  introduceSelf() {
-    console.log(`我是${this.name}，我是你们的${this.teaches}老师。`);
+  // New method to calculate and log the perimeter of the shape
+  calcPerimeter() {
+    const perimeter = this.sides * this.sideLength;
+    console.log(`The perimeter of ${this.name} is ${perimeter}`);
   }
 }
 
-class Student extends Person {
-  constructor(name, year) {
-    super(name);
-    this.year = year;
+class Square extends Shape {
+  constructor(sideLength) {
+    super("Square", 4, sideLength);
   }
 
-  introduceSelf() {
-    console.log(
-      `Hello, my name is ${this.name} and I am in year ${this.year}.`
-    );
-  }
-
-  canStudyArchery() {
-    return this.year > 1;
+  calcArea() {
+    const Area = this.sideLength * this.sideLength;
+    console.log(`Area is ${Area}`);
   }
 }
 
-// 示例用法
-const professor = new Professor("张老师", "数学");
-professor.introduceSelf(); // 输出："我是张老师，我是你们的数学老师。"
-professor.grade("论文"); // 调用评分逻辑
-
-const student = new Student("小明", 2);
-student.introduceSelf(); // 输出："Hello, my name is 小明 and I am in year 2."
-console.log(student.canStudyArchery()); // 输出：true
+const a1 = new Square(4);
+a1.calcArea();
+a1.calcPerimeter();
