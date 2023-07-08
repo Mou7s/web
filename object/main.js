f60e5857356b6fb25172a8d2921bf22b7cc8ef90
Mou7s <liuchuge7777777@gmail.com>
2023-07-08 19:49:40 +0800

    Update main.js

--- object/main.js
+++ object/main.js
@@ -92,7 +92,19 @@ class EvilCircle extends Shape {
     };
   }
 
-  collisionDetect() {}
+  collisionDetect() {
+    for (let j = 0; j < balls.length; j++) {
+      if (this !== balls[j]) {
+        const dx = this.x - balls[j].x;
+        const dy = this.y - balls[j].y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + balls[j].size) {
+          balls[j].color = this.color = randomColor();
+        }
+      }
+    }
+  }
 }
 
 Ball.prototype.draw = function () {
