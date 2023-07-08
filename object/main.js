ceb4e324fb0be6ea674bc8129c0e03e79f202968
Mou7s <liuchuge7777777@gmail.com>
2023-07-08 19:24:58 +0800

    1

--- object/main.js
+++ object/main.js
@@ -1,1 +1,169 @@
 import "./style.css";
+
+// 设置画布
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+// 生成随机数的函数
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function randomColor() {
+  return (
+    "rgb(" +
+    random(0, 255) +
+    ", " +
+    random(0, 255) +
+    ", " +
+    random(0, 255) +
+    ")"
+  );
+}
+class Shape {
+  constructor(x, y, velX, velY) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+  }
+}
+
+class Ball extends Shape {
+  constructor(x, y, velX, velY, color, size) {
+    super(x, y, velX, velY);
+    this.color = color;
+    this.size = size;
+    this.exists = true; // 新增的 exists 属性，默认为
+  }
+}
+
+class EvilCircle extends Shape {
+  constructor(x, y) {
+    super(x, y, 20, 20); // 继承 x, y, exists 并设定 velX 和 velY 为 20
+    this.color = "white"; // 新增的 color 属性，默认为 'white'
+    this.size = 10; // 新增的 size 属性，默认为 10
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.lineWidth = 3; // 设置线条的宽度为 3
+    ctx.strokeStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+  checkBounds() {
+    if (this.x + this.size >= width) {
+      this.x = width - this.size; // 将 x 的值修正为边界减去 size 的值
+    }
+
+    if (this.x - this.size <= 0) {
+      this.x = this.size; // 将 x 的值修正为 size 的值
+    }
+
+    if (this.y + this.size >= height) {
+      this.y = height - this.size; // 将 y 的值修正为边界减去 size 的值
+    }
+
+    if (this.y - this.size <= 0) {
+      this.y = this.size; // 将 y 的值修正为 size 的值
+    }
+  }
+
+  setControls() {
+    window.onkeydown = (e) => {
+      switch (e.key) {
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "d":
+          this.x += this.velX;
+          break;
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
+      }
+    };
+  }
+
+  collisionDetect() {}
+}
+
+Ball.prototype.draw = function () {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+};
+
+Ball.prototype.update = function () {
+  if (this.x + this.size >= width) {
+    this.velX = -this.velX;
+  }
+
+  if (this.x - this.size <= 0) {
+    this.velX = -this.velX;
+  }
+
+  if (this.y + this.size >= height) {
+    this.velY = -this.velY;
+  }
+
+  if (this.y - this.size <= 0) {
+    this.velY = -this.velY;
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+};
+
+Ball.prototype.collisionDetect = function () {
+  for (let j = 0; j < balls.length; j++) {
+    if (this !== balls[j]) {
+      const dx = this.x - balls[j].x;
+      const dy = this.y - balls[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].color = this.color = randomColor();
+      }
+    }
+  }
+};
+
+let balls = [];
+
+while (balls.length < 25) {
+  let size = random(10, 20);
+  let ball = new Ball(
+    // 为避免绘制错误，球至少离画布边缘球本身一倍宽度的距离
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomColor(),
+    size
+  );
+  balls.push(ball);
+}
+
+function loop() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.25 )";
+  ctx.fillRect(0, 0, width, height);
+
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].draw();
+    balls[i].update();
+    balls[i].collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
