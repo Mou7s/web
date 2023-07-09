bd0c72323f60341f4a9413da89c09fd85d13b76c
Mou7s <liuchuge7777777@gmail.com>
2023-07-09 22:12:17 +0800

    #

--- async/main.js
+++ async/main.js
@@ -1,8 +1,10 @@
-const button = document.querySelector("#set-alarm");
-button.addEventListener("click", setAlarm);
-
-function setAlarm() {
-  window.setTimeout(() => {
-    console.log("Wake up!");
-  }, 1000);
+function alarm(person, delay) {
+  return new Promise((resolve, reject) => {
+    if (delay < 0) {
+      throw new Error("Alarm delay must not be negative");
+    }
+    window.setTimeout(() => {
+      resolve(`Wake up, ${person}!`);
+    }, delay);
+  });
 }
