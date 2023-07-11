f59e7858f335bfbd57c673812d10088176474877
mou7s <liuchuge7777777@gmail.com>
2023-07-11 22:53:52 +0800

    #

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,19 +1,13 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  templateUrl: 'app.component.html',
+  styles: [],
 })
-export class AppComponent implements AfterViewInit {
-  message = 'My Message';
-  imageURL = 'https://i.picsum.photos/id/885/200/100.jpg';
-  ngAfterViewInit() {
-    setInterval(() => (this.message = Date.now().toString()), 1000);
-    setInterval(() => {
-      (this.imageURL =
-        'https://picsum.photos/200/100?random&t=' + Math.random()),
-        2000;
-    });
-  }
+export class AppComponent {
+  myColor = 'red';
+  myBackgroundColor = 'cyan';
+  myFontSizeEM = 3;
+  myFontSize = 50;
 }
