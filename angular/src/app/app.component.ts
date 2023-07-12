bd1cc1483a12cbeec405224650f186711f8e2b6b
mou7s <liuchuge7777777@gmail.com>
2023-07-12 21:15:40 +0800

    viewchild decorator

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,66 +1,37 @@
 import {
   Component,
-  AfterViewInit,
-  ViewChild,
-  AfterContentInit,
   OnInit,
+  AfterViewInit,
   ElementRef,
-  AfterViewChecked,
+  ViewChild,
 } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styles: [],
 })
-export class AppComponent
-  implements OnInit, AfterContentInit, AfterViewInit, AfterViewChecked
-{
+export class AppComponent implements OnInit, AfterViewInit {
   name: string = 'Murphy';
 
-  @ViewChild('title1', { static: false })
-  ctitle1!: ElementRef;
+  @ViewChild('title1', { static: false }) ctitle1!: ElementRef;
+  @ViewChild('title2', { static: false }) ctitle2!: ElementRef;
 
-  @ViewChild('title2', { static: true })
-  ctitle2!: ElementRef;
-
-  // 方法1
   ngOnInit() {
-    console.log('ctitle1 in ngOnInit : ' + this.getTitleValue(this.ctitle1));
-    console.log('ctitle2 in ngOnInit : ' + this.getTitleValue(this.ctitle2));
+    this.logTitleValues('ngOnInit');
   }
 
-  // 方法2
-  ngAfterContentInit() {
-    console.log(
-      'ctitle1 in ngAfterContentInit : ' + this.getTitleValue(this.ctitle1)
-    );
-    console.log(
-      'ctitle2 in ngAfterContentInit : ' + this.getTitleValue(this.ctitle2)
-    );
-  }
-
-  // 方法3
   ngAfterViewInit() {
-    console.log(
-      'ctitle1 in ngAfterViewInit : ' + this.getTitleValue(this.ctitle1)
-    );
-    console.log(
-      'ctitle2 in ngAfterViewInit : ' + this.getTitleValue(this.ctitle2)
-    );
+    this.logTitleValues('ngAfterViewInit');
   }
 
-  // 方法4
-  ngAfterViewChecked() {
-    console.log(
-      'ctitle1 in ngAfterViewChecked : ' + this.getTitleValue(this.ctitle1)
-    );
-    console.log(
-      'ctitle2 in ngAfterViewChecked : ' + this.getTitleValue(this.ctitle2)
-    );
+  private logTitleValues(hook: string) {
+    console.log(`---- ${hook} ----`);
+    console.log(`ctitle1: ${this.getTitleValue(this.ctitle1)}`);
+    console.log(`ctitle2: ${this.getTitleValue(this.ctitle2)}`);
   }
 
-  // 如果传入的元素不为空，则输出该元素的文本内容
-  getTitleValue(v: ElementRef) {
-    return v ? v.nativeElement.innerHTML : v;
+  private getTitleValue(element: ElementRef | undefined): string {
+    return element ? element.nativeElement.innerHTML : 'Element not found';
   }
 }
