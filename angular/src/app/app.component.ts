6a0776f994550c6e6640a365e5ea24b538cc3aae
Mou7s <liuchuge7777777@gmail.com>
2023-07-14 05:43:10 +0800

    #

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,18 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, of, from, range, timer, interval, map, defer } from 'rxjs';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import {
+  Observable,
+  of,
+  from,
+  range,
+  timer,
+  interval,
+  map,
+  defer,
+  tap,
+  filter,
+  concat,
+} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements AfterViewInit {
   constructor() {}
+  ngAfterViewInit(): void {
+    // 创建两个发出数字的 Observable
+    const numbersObservable1 = from([1, 2, 3]);
+    const numbersObservable2 = from([4, 5, 6]);
 
-  ngOnInit(): void {
-    timer(1000, 100) // 首次等待3s，然后每隔1s开始发出数据
-      .subscribe((value) => {
-        console.log(Math.random());
-      }); // 3s后开始输出0，然后每隔1s，依次输出1、2、3、……
+    // 使用 concat 操作符将两个 Observable 合并到一个 Observable 中
+    const numbersObservable = numbersObservable1.pipe(
+      concat(numbersObservable2)
+    );
+
+    // 订阅 Observable
+    numbersObservable.subscribe((x) => {
+      console.log(x);
+    });
   }
 }
