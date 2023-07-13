b08410b2a82c05f40b6bfff955a2df77b5d8d7da
Mou7s <liuchuge7777777@gmail.com>
2023-07-13 17:12:28 +0800

    #

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,32 +1,20 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Observable, interval, Subscription } from 'rxjs';
+import { Component } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit, OnDestroy {
-  timerValue: number = 0;
-  timerSubscription: Subscription | undefined;
-
-  ngOnInit() {
-    this.startTimer();
-  }
-
-  ngOnDestroy() {
-    this.stopTimer();
-  }
+export class AppComponent {
+  // 组件的属性和方法在这里定义
 
   startTimer() {
-    this.timerSubscription = interval(1000).subscribe(() => {
-      this.timerValue++;
-    });
-  }
-
-  stopTimer() {
-    if (this.timerSubscription) {
-      this.timerSubscription.unsubscribe();
-    }
+    of(1, 2, 3) // 创建3个数字的数据流
+      .subscribe({
+        next: (value: number) => console.log('next:', value),
+        error: (err: any) => console.log('error:', err),
+        complete: () => console.log('the end'),
+      });
   }
 }
