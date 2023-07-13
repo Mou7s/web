53d8399b166d959dbae53582b89b6cd1b1967235
Mou7s <liuchuge7777777@gmail.com>
2023-07-14 04:03:22 +0800

    random number

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,20 +1,18 @@
-import { Component } from '@angular/core';
-import { of } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of, from, range, timer, interval, map, defer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  // 组件的属性和方法在这里定义
+export class AppComponent implements OnInit {
+  constructor() {}
 
-  startTimer() {
-    of(1, 2, 3) // 创建3个数字的数据流
-      .subscribe({
-        next: (value: number) => console.log('next:', value),
-        error: (err: any) => console.log('error:', err),
-        complete: () => console.log('the end'),
-      });
+  ngOnInit(): void {
+    timer(1000, 100) // 首次等待3s，然后每隔1s开始发出数据
+      .subscribe((value) => {
+        console.log(Math.random());
+      }); // 3s后开始输出0，然后每隔1s，依次输出1、2、3、……
   }
 }
