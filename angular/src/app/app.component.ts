4a52339045fc6dd627f7ec3e01416658c9eb832e
mou7s <liuchuge7777777@gmail.com>
2023-07-14 18:59:32 +0800

    #

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,35 +1,11 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
-import {
-  Observable,
-  of,
-  from,
-  range,
-  timer,
-  interval,
-  map,
-  defer,
-  tap,
-  filter,
-  concat,
-  merge,
-  concatMap,
-  zip,
-  take,
-} from 'rxjs';
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements AfterViewInit {
-  constructor() {}
-
-  ngAfterViewInit(): void {
-    const observable = from(fetch('https://api.github.com/users/barda'));
-
-    observable.subscribe((data) => {
-      console.log(data);
-    });
-  }
+export class AppComponent {
+  favoriteColorControl = new FormControl('');
 }
