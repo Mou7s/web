481f67218536d3dd39298e6ac3904163dae04c60
mou7s <liuchuge7777777@gmail.com>
2023-07-14 20:57:01 +0800

    formsmodule

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
