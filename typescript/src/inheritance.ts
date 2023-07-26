d633b97a99207140a852d69f6308bb0861790b8f
Mou7s <liuchuge7777777@gmail.com>
2023-07-26 14:47:03 +0800

    #

--- typescript/src/inheritance.ts
+++ typescript/src/inheritance.ts
@@ -1,5 +1,6 @@
 class Button {
   public name: string;
+  protected shape: string = "rectanglar";
   constructor(name: string) {
     this.name = name;
   }
@@ -14,13 +15,17 @@ class Button {
 }
 
 class Link extends Button {
+  private href: string;
+
+  constructor(name: string, href: string) {
+    super(name);
+    this.href = href;
+  }
   public render() {
-    console.log(`<a href="">${this.name}</a>`);
+    console.log(`<a href="${this.href}" shape=${this.shape}>${this.name}</a>`);
   }
 }
 
-const link = new Link("click me to visit");
-
-link.onClick();
+const link = new Link("click me to visit", "https://www.google.com");
 
 link.render();
