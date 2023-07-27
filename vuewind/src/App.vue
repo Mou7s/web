9a229e6e3239493c3a698d0930415e0334839d90
mou7s <liuchuge7777777@gmail.com>
2023-07-27 22:40:19 +0800

    #

--- vuewind/src/App.vue
+++ vuewind/src/App.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 
-const count = ref(0);
+const counter = reactive({ count: 0 });
 
 const message = ref('Hello World!');
-
-console.log(count);
-count.value++;
 </script>
 
 <template>
-  <div class="grid place-content-center">
-    <h1 class="text-9xl">{{ message.split('').reverse().join('') }}</h1>
-    <p>count is: {{ count }}</p>
+  <div class="grid place-content-center text-center h-screen">
+    <h1 class="text-9xl">{{ message }}</h1>
+    <p>count is: {{ counter.count }}</p>
+
+    <button @click="counter.count++">Count++</button>
   </div>
 </template>
 
