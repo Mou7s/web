38b8742ed28f4f286b4b15ec0ea157cf174d6c7d
mou7s <liuchuge7777777@gmail.com>
2023-07-27 22:34:38 +0800

    #

--- vuewind/src/App.vue
+++ vuewind/src/App.vue
@@ -1,33 +1,19 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue';
+import { ref } from 'vue';
+
+const count = ref(0);
+
+const message = ref('Hello World!');
+
+console.log(count);
+count.value++;
 </script>
 
 <template>
-
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="grid place-content-center">
+    <h1 class="text-9xl">{{ message.split('').reverse().join('') }}</h1>
+    <p>count is: {{ count }}</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
