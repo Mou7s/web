4615ec7dfa04c3347559d15abbe1d503dfa1172e
Mou7s <liuchuge7777777@gmail.com>
2023-07-27 15:10:57 +0800

    #

--- vueTodolist/src/components/TodoFilter.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="flex my-6 mx-1 text-xs">
-        <span class="mr-1">All</span>
-        <span class="mr-1">Completed</span>
-        <span class="mr-1">Ongoing</span>
-    </div>
-</template>
--- vueTodolist/src/components/TodoList.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import TodoListItem from './TodoListItem.vue';
-</script>
-
-<template>
-    <div class="grid gap-y-4 ">
-        <TodoListItem v-for="n in 3" :key="n">Todo{{ n }}</TodoListItem>
-    </div>
-</template>
