57fda0bbe9d0810ce9d02e50acc9ca23448d8796
Mou7s <liuchuge7777777@gmail.com>
2023-07-28 08:40:03 +0800

    #

--- vuewind/src/App.vue
+++ vuewind/src/App.vue
@@ -1,19 +1,55 @@
 <script setup lang="ts">
-import { ref, reactive } from 'vue';
+import { ref } from 'vue';
+const newTodo = ref('');
+const todos = ref([]);
 
-const counter = reactive({ count: 0 });
+function addTodo() {
+  if (newTodo.value.trim() !== '') {
+    todos.value.push(newTodo.value.trim());
+    newTodo.value = '';
+  }
+}
 
-const message = ref('Hello World!');
+function removeTodo(index: number) {
+  todos.value.splice(index, 1);
+}
 </script>
 
 <template>
-  <div class="grid place-content-center text-center h-screen">
-    <h1 class="text-9xl">{{ message }}</h1>
+  <main class="grid min-h-screen w-screen text-center place-content-center">
+    <div id="container" class="bg-cyan-50 rounded-2xl">
+      <p class="text-3xl">todo list</p>
+      <div class="relative">
+        <input
+          type="text"
+          v-model="newTodo"
+          @keyup.enter="addTodo"
+          placeholder="input here"
+          class="rounded-2xl text-center"
+        />
+        <button
+          @click="addTodo"
+          class="absolute right-0 w-6 h-6 rounded-full bg-cyan-100"
+        >
+          +
+        </button>
+      </div>
 
-    <button @click="counter.count++" class="rounded-3xl">
-      count is: {{ counter.count }}
-    </button>
-  </div>
+      <ul>
+        <div class="relative rounded-3xl">
+          <li v-for="(todo, index) in todos" :key="index">
+            {{ todo }}
+            <button
+              class="w-6 h-6 absolute right-0 rounded-full bg-cyan-100"
+              @click="removeTodo(index)"
+            >
+              X
+            </button>
+          </li>
+        </div>
+      </ul>
+    </div>
+  </main>
 </template>
 
 <style scoped></style>
