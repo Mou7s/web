59dd9a44b521d610230d20b2f49e2b4bf6c4b271
Mou7s <liuchuge7777777@gmail.com>
2023-07-29 04:15:10 +0800

    #

--- vuewind/src/App.vue
+++ vuewind/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue';
 const todolist = ref(['learn vue', 'learn javascript']);
+const ongoing = ref(['learn html', 'learn css']);
 const inputvalue = ref('');
 
 const addtodo = () => {
@@ -10,27 +11,50 @@ const addtodo = () => {
   }
 };
 
-const deleteTodo = (index: number) => {
+const addongoing = (todo, index) => {
+  ongoing.value.push(todo);
   todolist.value.splice(index, 1);
 };
 </script>
 
 <template>
-  <div class="grid place-content-center w-full min-h-screen text-center">
-    <h1 class="text-6xl">todoLists</h1>
-    <div class="relative">
-      <input class="rounded" v-model="inputvalue" />
-      <button class="absolute right-0 bg-cyan-500 w-6" @click="addtodo()">
-        +
-      </button>
-    </div>
+  <div class="grid grid-cols-3 place-content-center w-full text-center">
+    <!-- Todo 列表 -->
 
-    <div v-for="(todo, index) of todolist" :key="index">
+    <div>
+      <h1 class="text-3xl">Todo</h1>
       <div class="relative">
-        <span>{{ todo }}</span>
-        <button class="absolute right-0" @click="deleteTodo(index)">X</button>
+        <input class="rounded" v-model="inputvalue" />
+        <button class="absolute right-0 bg-cyan-500 w-6" @click="addtodo()">
+          +
+        </button>
+      </div>
+
+      <div v-for="(todo, index) of todolist" :key="index">
+        <div class="relative">
+          <span>{{ todo }}</span>
+
+          <button
+            class="absolute right-0 bg-cyan-500 w-6"
+            @click="addongoing(todo, index)"
+          >
+            ->
+          </button>
+        </div>
       </div>
     </div>
+    <!-- 进行中列表 -->
+
+    <div class="w-full">
+      <h1 class="text-3xl mb-8">ongoing</h1>
+
+      <div v-for="(item, index) of ongoing" :key="index">
+        <p>{{ item }}</p>
+      </div>
+    </div>
+    <!-- 已完成列表 -->
+
+    <div class="w-full text-3xl"><h1>completed</h1></div>
   </div>
 </template>
 
