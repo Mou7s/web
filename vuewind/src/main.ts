bafb676295e275da02520ae591fdc2f711decf47
Mou7s <liuchuge7777777@gmail.com>
2023-08-01 07:54:48 +0800

    #

--- vuewind/src/main.ts
+++ vuewind/src/main.ts
@@ -1,7 +1,5 @@
 import { createApp } from 'vue';
 import './style.css';
-import todoapp from './todoApp.vue';
-import typescript from './typescript.vue';
 import test from './test.vue';
 
 createApp(test).mount('#app');
