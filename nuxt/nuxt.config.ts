bd7854ce9595139b287e725f9a4ae6f4a038b250
Mou7s <liuchuge7777777@gmail.com>
2023-08-03 04:04:00 +0800

    #

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,6 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import components from 'unplugin-vue-components/vite';
+import autoImport from 'unplugin-auto-import/vite';
+import { VarletUIResolver } from 'unplugin-vue-components/resolvers';
+import { defineNuxtConfig } from 'nuxt/config';
+
 export default defineNuxtConfig({
-  // pages: true,
-  modules: ['@nuxtjs/tailwindcss'],
+  vite: {
+    ssr: {
+      noExternal: ['@varlet/ui'],
+    },
+    plugins: [
+      components({
+        resolvers: [VarletUIResolver()],
+      }),
+      autoImport({
+        resolvers: [VarletUIResolver({ autoImport: true })],
+      }),
+    ],
+  },
+  modules: ['@nuxtjs/tailwindcss', '@varlet/nuxt'],
   devtools: { enabled: true },
 });
--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,14 +1,34 @@
+<script setup lang="ts">
+import { StyleProvider, Themes } from '@varlet/ui';
+import { ref } from 'vue';
+
+let currentTheme = null;
+function toggleTheme() {
+  currentTheme = currentTheme ? null : Themes.dark;
+  StyleProvider(currentTheme);
+}
+</script>
+
 <template>
   <header class="sticky top-0 text-center">
     <nav class="flex justify-around">
-      <div>
-        <NuxtLink to="/"><span class="font-bold">Home</span></NuxtLink>
-      </div>
-      <div>
-        <NuxtLink to="/laptop"><span class="font-bold">Laptop</span></NuxtLink>
-      </div>
-      <div></div>
+      <NuxtLink to="/">
+        <VarButton>Home</VarButton>
+      </NuxtLink>
+      <NuxtLink to="/laptop">
+        <VarButton>Laptop</VarButton>
+      </NuxtLink>
+
+      <var-button @click="toggleTheme">Theme</var-button>
     </nav>
   </header>
   <div class="min-h-screen">index</div>
 </template>
+
+<style scoped>
+body {
+  transition: background-color 0.25s, color 0.25s;
+  color: var(--color-text);
+  background-color: var(--color-body);
+}
+</style>
