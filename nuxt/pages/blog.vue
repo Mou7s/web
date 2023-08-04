d0a5fba5cd549b0cf13db3e7c2a503f220731e5d
mou7s <liuchuge7777777@gmail.com>
2023-08-04 10:12:13 +0800

    #

--- nuxt/pages/blog.vue
+++ nuxt/pages/blog.vue
@@ -4,10 +4,20 @@ import Header from './header.vue';
 
 <template>
   <Header></Header>
+  <div class="bg-blue-100 min-h-screen gird">
+    <div class="mx-8">
+      <var-breadcrumbs>
+        <NuxtLink to="/"><var-breadcrumb>Home</var-breadcrumb></NuxtLink>
+        <NuxtLink to="/blog"><var-breadcrumb>blog</var-breadcrumb></NuxtLink>
+      </var-breadcrumbs>
+    </div>
 
-  <var-breadcrumbs>
-    <NuxtLink to="/"><var-breadcrumb>Home</var-breadcrumb></NuxtLink>
-    <NuxtLink to="/blog"><var-breadcrumb>blog</var-breadcrumb></NuxtLink>
-  </var-breadcrumbs>
-  some blogs
+    <div class="rounded-full bg-white w-5/6 my-0 mx-auto">
+      <h1 class="font-bold text-2xl">如何使用throttleStop</h1>
+
+      <p>
+        ThrottLestop是一个小型应用程序，旨在监视和纠正许多笔记本电脑上使用的CPU性能配置。
+      </p>
+    </div>
+  </div>
 </template>
---
+++ nuxt/pages/button.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const props = defineProps(['to']);
+</script>
+
+<template>
+  <NuxtLink :to="props.to">
+    <div
+      class="rounded-full h-16 w-60 grid place-content-center hover:bg-slate-300"
+    >
+      <slot></slot>
+    </div>
+  </NuxtLink>
+</template>
