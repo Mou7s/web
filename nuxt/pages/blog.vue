29693a4d952a55c9278578271c5eb5ee05ce0d60
mou7s <liuchuge7777777@gmail.com>
2023-08-08 20:43:08 +0800

    #

--- nuxt/pages/blog.vue
+++ nuxt/pages/blog.vue
@@ -10,19 +10,16 @@ useSeoMeta({
 });
 </script>
 <template>
-  <div class="grid grid-cols-5">
-    <Nav></Nav>
-    <div class="bg-blue-100 min-h-screen col-span-4">
-      <div class="grid h-16">
-        <var-breadcrumbs>
-          <NuxtLink to="/"><var-breadcrumb></var-breadcrumb></NuxtLink>
-          <NuxtLink to="/blog"><var-breadcrumb>blog</var-breadcrumb></NuxtLink>
-        </var-breadcrumbs>
-      </div>
+  <div class="bg-blue-100 min-h-screen">
+    <div class="grid h-16">
+      <var-breadcrumbs>
+        <NuxtLink to="/"><var-breadcrumb></var-breadcrumb></NuxtLink>
+        <NuxtLink to="/blog"><var-breadcrumb>blog</var-breadcrumb></NuxtLink>
+      </var-breadcrumbs>
+    </div>
 
-      <div class="rounded-3xl w-5/6 my-0 mx-auto bg-white p-10">
-        <Vsc></Vsc>
-      </div>
+    <div class="rounded-3xl w-5/6 my-0 mx-auto bg-white p-10">
+      <Vsc></Vsc>
     </div>
   </div>
 </template>
