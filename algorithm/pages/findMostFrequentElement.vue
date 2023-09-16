b7b05e056586cdc11cf3f9fb528290f068eabf76
mou7s <liuchuge7777777@gmail.com>
2023-09-16 18:35:43 +0800

    #

---
+++ algorithm/pages/findMostFrequentElement.vue
@@ -0,0 +1,36 @@
+<script setup>
+function findMostFrequentElement(arr) {
+  const elementCount = {};
+  let mostFrequentElement = arr[0];
+  let maxCount = 1;
+
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+
+    if (elementCount[element]) {
+      elementCount[element].count++;
+      elementCount[element].positions.push(i);
+    } else {
+      elementCount[element] = { count: 1, positions: [i] };
+    }
+
+    if (elementCount[element].count > maxCount) {
+      mostFrequentElement = element;
+      maxCount = elementCount[element].count;
+    }
+  }
+
+  return {
+    mostFrequentElement: mostFrequentElement,
+    positions: elementCount[mostFrequentElement].positions,
+  };
+}
+
+const array = [1, 2, 2, 3, 4, 2, 5, 6, 2];
+const result = findMostFrequentElement(array);
+</script>
+<template>
+  array:{{ array }}
+  <p>most frequent element: {{ result.mostFrequentElement }}</p>
+  <p>index:{{ result.positions.join(', ') }}</p>
+</template>
--- algorithm/pages/index.vue
+++ algorithm/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <p class="text-5xl font-bold">hello world</p>
   <NuxtLink to="/twoSum"><button>twoSum</button></NuxtLink>
-  <NuxtLink to="/getType"><button>getType</button></NuxtLink>
-
+  <NuxtLink to="/findMostFrequentElement"
+    ><button>findMostFrequentElement</button></NuxtLink
+  >
 </template>
