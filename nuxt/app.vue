7acdbf498f9591f2ef002bc2f0cd27d21e8c8864
mou7s <liuchuge7777777@gmail.com>
2023-11-07 17:10:55 +0800

    #

--- nuxt/app.vue
+++ nuxt/app.vue
@@ -1,52 +1,5 @@
-<template>
-  <div>
-    <h1>堆排序</h1>
-    <p>排序前：{{ beforeSort }}</p>
-    <p>排序后：{{ afterSort }}</p>
-  </div>
-</template>
+<template></template>
 
 <script setup>
-function heapSort(arr) {
-  let len = arr.length;
-
-  // 构建大顶堆
-  for (let i = Math.floor(len / 2) - 1; i >= 0; i--) {
-    heapify(arr, i, len);
-  }
-
-  // 交换堆顶元素和末尾元素，重新构造堆
-  for (let i = len - 1; i > 0; i--) {
-    [arr[0], arr[i]] = [arr[i], arr[0]];
-    heapify(arr, 0, i);
-  }
-
-  return arr;
-}
-
-function heapify(arr, i, len) {
-  let left = 2 * i + 1;
-  let right = 2 * i + 2;
-  let largest = i;
-
-  if (left < len && arr[left] > arr[largest]) {
-    largest = left;
-  }
-
-  if (right < len && arr[right] > arr[largest]) {
-    largest = right;
-  }
-
-  if (largest !== i) {
-    [arr[i], arr[largest]] = [arr[largest], arr[i]];
-    heapify(arr, largest, len);
-  }
-}
-const arr = [4, 3, 8, 5, 9];
-
-const beforeSort = arr.join(', ');
-const afterSort = heapSort(arr).join(', ');
-
-console.log(beforeSort);
-console.log(afterSort);
+import './public/test';
 </script>
---
+++ nuxt/public/test.js
@@ -0,0 +1,8 @@
+const spinWords = (string) => {
+    return string
+        .split(' ')
+        .map((word) => {
+            return word.length >= 5 ? word.split('').reverse().join('') : word;
+        })
+        .join(' ');
+}
