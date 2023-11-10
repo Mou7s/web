a4cb03a03d216dc7d218e2177c0075bc5cb86618
mou7s <liuchuge7777777@gmail.com>
2023-11-10 18:31:14 +0800

    #

--- nuxt/app.vue
+++ nuxt/app.vue
@@ -1,5 +1,30 @@
-<template></template>
+<template>
+  <nav>
+    <NuxtLink to="/"> Home </NuxtLink>
+    <NuxtLink to="/custom"> Custom </NuxtLink>
+    <NuxtLink to="/dynamic"> Dynamic </NuxtLink>
+    <NuxtLink to="/other"> Other </NuxtLink>
+  </nav>
+  <NuxtLayout class="layouts">
+    <NuxtPage />
+  </NuxtLayout>
+  <footer>
+    <button @click="setPageLayout('default')">layouts/default.vue</button>
+    <button @click="setPageLayout('custom')">layouts/custom.vue</button>
+    <button @click="setPageLayout('other')">layouts/other.vue</button>
+    <button @click="setPageLayout(false)">Remove layout</button>
+  </footer>
+</template>
 
-<script setup>
-import './public/test';
-</script>
+<style scoped>
+nav,
+footer {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  padding: 1rem;
+}
+.layouts {
+  padding: 1rem;
+}
+</style>
