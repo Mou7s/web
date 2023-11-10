b862b2c4a6033318802b0c9fe2c51db19345448e
mou7s <liuchuge7777777@gmail.com>
2023-11-10 20:06:21 +0800

    #

--- starter-ui/app.vue
+++ starter-ui/app.vue
@@ -3,20 +3,22 @@
     <UCard class="mt-10">
       <template #header>
         <div class="flex justify-between">
-          <h1>Welcome to Nuxt UI Starter</h1>
-          <ColorScheme
-            ><USelect
+          <h1>Welcome to cpus benchmarks score</h1>
+          <ColorScheme>
+            <USelect
               v-model="$colorMode.preference"
               :options="['system', 'light', 'dark']"
-          /></ColorScheme>
+            />
+          </ColorScheme>
         </div>
       </template>
       <UButton
         icon="i-heroicons-book-open"
         to="https://ui.nuxt.com"
         target="_blank"
-        >Open Nuxt UI Documentation</UButton
-      >
+        >Open Nuxt UI Documentation
+      </UButton>
     </UCard>
+    <NuxtPage></NuxtPage>
   </UContainer>
 </template>
---
+++ starter-ui/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <UCard class="mt-10"><p>details</p> </UCard>
+</template>
