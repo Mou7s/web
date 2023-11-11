def125765b1bcbebb24014eb7023c253b2fe4b81
mou7s <liuchuge7777777@gmail.com>
2023-11-11 12:37:41 +0800

    #

--- starter-ui/app.vue
+++ starter-ui/app.vue
@@ -1,23 +1,46 @@
+<script setup>
+const colorMode = useColorMode();
+const date = ref(new Date());
+
+const isDark = computed({
+  get() {
+    return colorMode.value === 'dark';
+  },
+  set() {
+    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark';
+  },
+});
+
+const label = computed(() =>
+  date.value.toLocaleDateString('en-us', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  })
+);
+
+console.log(label.value);
+</script>
+
 <template>
   <UContainer>
     <UCard class="mt-10">
-      <template #header>
-        <div class="flex justify-between">
-          <h1>Welcome to cpus benchmarks score</h1>
-          <ColorScheme>
-            <USelect
-              v-model="$colorMode.preference"
-              :options="['system', 'light', 'dark']"
-            />
-          </ColorScheme>
-        </div>
-      </template>
-      <UButton
-        icon="i-heroicons-book-open"
-        to="https://ui.nuxt.com"
-        target="_blank"
-        >Open Nuxt UI Documentation
-      </UButton>
+      <div class="flex justify-between">
+        <UButton icon="i-heroicons-calendar-days-20-solid" :label="label" />
+        <ClientOnly>
+          <UButton
+            :icon="
+              isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'
+            "
+            color="gray"
+            variant="ghost"
+            aria-label="Theme"
+            @click="isDark = !isDark"
+          >
+          </UButton>
+        </ClientOnly>
+      </div>
     </UCard>
     <NuxtPage></NuxtPage>
   </UContainer>
