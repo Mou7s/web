8fd38166d0fe8e324d1c809302ff16631159e7f4
mou7s <liuchuge7777777@gmail.com>
2023-11-23 00:04:09 +0800

    #

--- nuxt/app.vue
+++ nuxt/app.vue
@@ -1,5 +1,28 @@
+<!-- ParentComponent.vue -->
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>Parent Component</h1>
+    <p>Parent Data: {{ parentData }}</p>
+    <ChildComponent />
   </div>
 </template>
+
+<script >
+import ChildComponent from '../components/ChildComponent.vue';
+
+export default {
+  components: {
+    ChildComponent,
+  },
+  setup() {
+    const parentData = ref('Hello from Parent');
+
+    // 使用 provide 将数据提供给子组件
+    provide('parentData', parentData);
+
+    return {
+      parentData,
+    };
+  },
+};
+</script>
