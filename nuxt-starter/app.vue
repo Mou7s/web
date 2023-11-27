a5eea8419d8e3646c5f0ab138d520f98c0bb3ea3
mou7s <liuchuge7777777@gmail.com>
2023-11-27 18:26:49 +0800

    #

---
+++ nuxt-starter/app.vue
@@ -0,0 +1,51 @@
+<script setup>
+const colorMode = useColorMode();
+const date = ref(new Date());
+
+const isDark = computed({
+  get() {
+    return colorMode.value === 'dark';
+  },
+  set() {
+    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark';
+  },
+});
+
+const label = computed(() =>
+  date.value.toLocaleDateString('en-us', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  })
+);
+</script>
+
+<template>
+  <UContainer>
+    <NuxtLoadingIndicator />
+
+    <UCard class="mt-10">
+      <div class="flex justify-between">
+        <UButton icon="i-heroicons-calendar-days-20-solid" :label="label" />
+        <div>
+          <ClientOnly>
+            <UButton
+              :icon="
+                isDark
+                  ? 'i-heroicons-moon-20-solid'
+                  : 'i-heroicons-sun-20-solid'
+              "
+              color="gray"
+              variant="ghost"
+              aria-label="Theme"
+              @click="isDark = !isDark"
+            >
+            </UButton>
+          </ClientOnly>
+        </div>
+      </div>
+    </UCard>
+    <NuxtPage></NuxtPage>
+  </UContainer>
+</template>
