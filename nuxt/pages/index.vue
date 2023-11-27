266841f776e7a85207053cbeb113e8b5def7c9b1
mou7s <liuchuge7777777@gmail.com>
2023-11-27 19:56:13 +0800

    #

---
+++ nuxt/pages/index.vue
@@ -0,0 +1,45 @@
+<script setup>
+let password = 'admin';
+let admin = 'admin';
+
+const state = () =>
+  useState({
+    email: admin,
+    password: admin,
+  });
+
+const validate = (state) => {
+  const errors = [];
+  if (!state.email) {
+    errors.push({ path: 'email', message: 'Required' });
+  }
+  if (!state.password) {
+    errors.push({ path: 'password', message: 'Required' });
+  }
+  if (state.password) {
+  }
+};
+
+async function onSubmit(event) {}
+</script>
+
+<template>
+  <UCard class="mt-10">
+    <UForm
+      :validate="validate"
+      :state="state"
+      class="space-y-4"
+      @submit="onSubmit"
+    >
+      <UFormGroup label="Email" name="email">
+        <UInput v-model="state.email" />
+      </UFormGroup>
+
+      <UFormGroup label="Password" name="password">
+        <UInput v-model="state.password" type="password" />
+      </UFormGroup>
+
+      <UButton type="submit"> Submit </UButton>
+    </UForm>
+  </UCard>
+</template>
