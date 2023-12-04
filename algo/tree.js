ee8a898e9a2c05c73d98bab6e764564b1ce163bb
mou7s <liuchuge7777777@gmail.com>
2023-12-04 23:31:50 +0800

    #

--- algo/tree.js
+++ algo/tree.js
@@ -76,9 +76,9 @@ class BinaryTree {
 // Usage example:
 const tree = new BinaryTree();
 tree.insert(7);
+tree.insert(4);
 tree.insert(6);
 tree.insert(5);
-tree.insert(4);
 tree.insert(3);
 tree.insert(2);
 tree.insert(1);
