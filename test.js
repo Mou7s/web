86538e7538f78c299f7e6852774c6bef3122079e
mou7s <liuchuge7777777@gmail.com>
2023-12-04 12:59:03 +0800

    #

--- test.js
+++ test.js
@@ -1,9 +1,43 @@
-function ipsBetween(start, end) {
-  let startIp = start.split('.');
-  let endIp = end.split('.');
-  let result = 0;
-  for (let i = 0; i < 4; i++) {
-    result += (endIp[i] - startIp[i]) * Math.pow(256, 3 - i);
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
   }
-  return result;
 }
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  add(data) {
+    const newNode = new Node(data);
+
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  print() {
+    let current = this.head;
+    let result = '';
+    while (current !== null) {
+      result += current.data + ' ';
+      current = current.next;
+    }
+    console.log(result);
+  }
+}
+
+// Usage:
+const linkedList = new LinkedList();
+linkedList.add(1);
+linkedList.add(2);
+linkedList.add(3);
+linkedList.print();
