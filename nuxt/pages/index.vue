9ee646b6a73ac390103ca98170fa14ac08478f55
mou7s <liuchuge7777777@gmail.com>
2023-12-08 16:11:45 +0800

    #

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,15 +1,50 @@
+<!--
+现在我们将元素的 attribute / property 响应式地绑定到状态上。
+这个 :title 语法是 v-bind:title 的简写。
+-->
+
 <script setup>
-let canvas = ref(null);
+const message = ref('Hello World!');
+const isRed = ref(true);
+const color = ref('green');
 
-onMounted(() => {
-  const ctx = canvas.value.getContext('2d');
+function toggleRed() {
+  isRed.value = !isRed.value;
+}
 
-  ctx.fillStyle = 'blue';
-  ctx.fillRect(10, 10, 150, 100);
-  ctx.fillRect(200, 200, 150, 100);
-});
+function toggleColor() {
+  color.value = color.value === 'green' ? 'blue' : 'green';
+}
 </script>
 
 <template>
-  <canvas ref="canvas"></canvas>
+  <p>
+    <span :title="message">
+      Hover your mouse over me for a few seconds to see my dynamically bound
+      title!
+    </span>
+  </p>
+
+  <!--
+  除了普通字符串之外，
+  class 绑定还特别支持了对象和数组
+  -->
+  <p :class="{ red: isRed }" @click="toggleRed">
+    This should be red... but click me to toggle it.
+  </p>
+
+  <!-- 
+    样式绑定也支持对象和数组
+    在Vue.js中，你可以使用v-bind:style或简写为:style来动态地绑定样式。
+    你可以传递一个对象给:style，对象的属性名是样式名，属性值是要应用的样式值。
+   -->
+  <p :style="{ color }" @click="toggleColor">
+    This should be green, and should toggle between green and blue on click.
+  </p>
 </template>
+
+<style>
+.red {
+  color: red;
+}
+</style>
