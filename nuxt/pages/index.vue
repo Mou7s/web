f4813f0af326184ca92abe11f69f0d95989c4e1c
mou7s <liuchuge7777777@gmail.com>
2023-12-09 14:05:53 +0800

    #

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -3,17 +3,17 @@
 -->
 
 <script setup>
-import { ref } from 'vue';
-
 const show = ref(true);
 const list = ref([1, 2, 3]);
 </script>
 
 <template>
-  <button @click="show = !show">Toggle List</button>
-  <button @click="list.push(list.length + 1)">Push Number</button>
-  <button @click="list.pop()">Pop Number</button>
-  <button @click="list.reverse()">Reverse List</button>
+  <div class="grid grid-flow-col">
+    <button @click="show = !show">Toggle List</button>
+    <button @click="list.push(list.length + 1)">Push Number</button>
+    <button @click="list.pop()">Pop Number</button>
+    <button @click="list.reverse()">Reverse List</button>
+  </div>
 
   <ul v-if="show && list.length">
     <li v-for="item of list">{{ item }}</li>
