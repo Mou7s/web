060015366835e731bc229e99917c469e2897ffd5
mou7s <liuchuge7777777@gmail.com>
2023-12-13 09:25:09 +0800

    #

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,20 +1,3 @@
-<script setup>
-const Sort = (array) => {
-  //bubblesort
-  for (let i = 0; i < array.length; i++) {
-    let swapped = false;
-    for (let j = 0; j < array.length - i - 1; j++) {
-      if (array[j] > array[j + 1]) {
-        let temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
-        swapped = true;
-      }
-    }
-    if (swapped == false) return array;
-  }
-  return array;
-};
-</script>
-
-<template>{{ Sort([7, 3, 5, 5, 6, 0, 8]) }}</template>
+<template>
+  <h2>Welcome to Layouts example</h2>
+</template>
---
+++ nuxt/pages/other.vue
@@ -0,0 +1,9 @@
+<script setup>
+definePageMeta({
+  middleware: 'other',
+});
+</script>
+
+<template>
+  <p>The layout is set within <code>middleware/other.ts</code>.</p>
+</template>
