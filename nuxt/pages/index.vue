6303a597db7ad9f044c54d44b43252866cd0fc80
mou7s <liuchuge7777777@gmail.com>
2024-01-03 16:08:26 +0800

    Update index.vue with new text size and add sleep function

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,3 +1,22 @@
 <template>
-  <div class="min-h-screen grid place-content-center text-9xl">Hello World</div>
+  <div class="min-h-screen grid place-content-center text-3xl">Hello World</div>
 </template>
+
+<script setup>
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, millis);
+  });
+}
+
+const example = async () => {
+  console.log('Hello');
+  await sleep(1000);
+  console.log('World');
+};
+example()
+</script>
