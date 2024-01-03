5f92a11c2f1af8839d6f8092f17bc5c29e8e6aaa
mou7s <liuchuge7777777@gmail.com>
2024-01-03 14:57:17 +0800

    Update dependencies in package.json and remove unused styles

--- nuxt/app.vue
+++ nuxt/app.vue
@@ -2,14 +2,3 @@
   <NuxtLoadingIndicator />
   <NuxtPage />
 </template>
-<style>
-body {
-  padding-bottom: 40px;
-  /* margin-top: 40px; */
-}
-
-/* 用户限制用户只能在y轴上滑动 */
-/* * {
-  touch-action: pan-y !important;
-}  */
-</style>
