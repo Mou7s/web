23f41db84ebb8ad7701692b950a0c65fd7e77688
mou7s <liuchuge7777777@gmail.com>
2024-01-10 01:42:10 +0800

    Update dependencies and add demo button

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,19 +1,29 @@
 <template>
   <div class="min-h-screen grid place-content-center text-3xl">
     <div :class="textColorClass" class="text-9xl font-serif">
-      {{ selectedColor }}
+      <h1 :class="textColor">Demo</h1>
+      <button :disabled="btnDisable" @click="btnDisable = true">
+        click once
+      </button>
     </div>
-    <select v-model="selectedColor">
-      <option value="blue">blue</option>
+    <select v-model="textColor">
       <option value="red">red</option>
+
+      <option value="blue">blue</option>
     </select>
   </div>
 </template>
 
 <script setup>
-const selectedColor = ref('blue');
-
-const textColorClass = computed(() => {
-  return `text-${selectedColor.value}-500`;
-});
+const textColor = ref('red');
+const btnDisable = ref(false);
 </script>
+
+<style>
+.red {
+  color: red;
+}
+.blue {
+  color: blue;
+}
+</style>
