c7adffd5b4de606165c3dd00b3d8a4c230976970
mou7s <liuchuge7777777@gmail.com>
2024-01-10 12:31:36 +0800

    Update index.vue template

---
+++ nuxt/pages/details/[id].vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <h1>Details</h1>
+    <p>id: {{ id }}</p>
+  </div>
+</template>
+<script setup>
+const route = useRoute();
+const id = route.params.id;
+</script>
