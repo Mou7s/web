f9eecc3370c687bc7eae1ef88b6cf8bca272f57c
mou7s <liuchuge7777777@gmail.com>
2024-01-11 16:24:59 +0800

    Add navigation and layout options to app.vue, delete details/[id].vue, and update index.vue

---
+++ nuxt/layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="border-green-200 border border-solid p-4 relative bg-lime-700 before:absolute default"
+  >
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.default::before {
+  content: 'layouts/default.vue';
+  position: absolute;
+  top: 2px;
+  left: 5px;
+  color: #68b538;
+  font-family: monospace;
+}
+</style>
