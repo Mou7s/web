bfc305a0ca0dbac61662c00831ce9170da1af96a
mou7s <liuchuge7777777@gmail.com>
2024-01-18 03:43:17 +0800

    Update dependencies and modify App.vue

--- vue2/src/App.vue
+++ vue2/src/App.vue
@@ -1,23 +1,33 @@
 <template>
   <div>
     <div class="container">
-      {{ msg }}
+      {{ data }}
+      <p> {{ msg }}
+      </p>
     </div>
     <button @click="clickContainer">Click me</button>
+    <a :href="url">
+      <p>route to valve</p>
+    </a>
+
   </div>
 </template>
 
 
 <script setup>
+import { onMounted } from 'vue'
+
+var data = { a: 1 }
+let url = "https://www.valvesoftware.com"
+
 const msg = 'Welcome to Your Vue.js App'
 const clickContainer = () => {
   console.log('clickContainer')
 }
+
+onMounted(() => {
+  url = 'https://www.google.com'
+})
+
 </script>
-<style>
-.container {
-  display: flex;
-  justify-content: center;
-  min-height: 100%;
-}
-</style>
+<style></style>
