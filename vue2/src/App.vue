61b41d2c8a62f4cddb4083e1a2bf6d9d6b6c0b81
mou7s <liuchuge7777777@gmail.com>
2024-01-17 16:32:34 +0800

    Refactor App.vue component structure

--- vue2/src/App.vue
+++ vue2/src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    {{ msg }}
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+const msg = 'Welcome to Your Vue.js App'
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+  display: flex;
+  justify-content: center;
+  min-height: 100%;
 }
 </style>
