ab03ccd83081dfa2403962e26de80b388a09b7ee
mou7s <liuchuge7777777@gmail.com>
2024-01-17 16:40:21 +0800

    Refactor App.vue template structure and add clickContainer function

--- vue2/src/App.vue
+++ vue2/src/App.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="container">
-    {{ msg }}
+  <div>
+    <div class="container">
+      {{ msg }}
+    </div>
+    <button @click="clickContainer">Click me</button>
   </div>
 </template>
 
 
 <script setup>
 const msg = 'Welcome to Your Vue.js App'
+const clickContainer = () => {
+  console.log('clickContainer')
+}
 </script>
 <style>
 .container {
