95ca5d4e78af4b8201786c6228708dce935d9a9c
mou7s <liuchuge7777777@gmail.com>
2024-03-03 23:35:11 +0800

    Add link to test page layout

--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="grid place-content-center h-screen">
     <UButton @click="setPageLayout('debounce')">Link to debounce</UButton>
+    <UButton @click="setPageLayout('test')">Link to test</UButton>
   </div>
 </template>
---
+++ nuxt/layouts/test.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="grid place-content-center h-screen">
+    <img src="https://picsum.photos/200/300" alt="" srcset="" />
+  </div>
+</template>
+<script setup></script>
