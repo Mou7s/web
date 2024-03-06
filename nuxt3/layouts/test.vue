50c6e2be9f01783127f40e58c2813ada45e7eceb
mou7s <liuchuge7777777@gmail.com>
2024-03-07 03:19:54 +0800

    Refactor longestCommonPrefix function in test.vue

--- nuxt3/layouts/test.vue
+++ nuxt3/layouts/test.vue
@@ -1,4 +1,28 @@
-<template>
-  <div>test</div>
-</template>
-<script setup></script>
+<template>{{ longestCommonPrefix(arr1) }}</template>
+
+<script setup>
+const longestCommonPrefix = () => {
+  const arr1 = ['flower', 'flow', 'flight'];
+  let prefix = '';
+
+  // for (let i = 0; i < arr1[0].length; i++) {
+  //   const char = arr1[0][i];
+
+  //   for (let j = 1; j < arr1.length; j++) {
+  //     if (arr1[j][i] !== char) return prefix;
+  //   }
+  //   prefix = prefix + char;
+  // }
+  // return prefix;
+
+  for (let i = 0; i < arr1[0].length; i++) {
+    const char = arr1[0][i];
+
+    for (let j = 1; j < arr1.length; j++) {
+      if (arr1[j][i] !== char) return prefix;
+    }
+    prefix = prefix + char;
+  }
+  return prefix;
+};
+</script>
