32890ab052fd61b9dbfbadbb69da46f9cd47c4eb
mou7s <liuchuge7777777@gmail.com>
2024-03-07 18:51:40 +0800

    Refactor code: Updated longestCommonPrefix function and added twoSum function

--- nuxt3/layouts/test.vue
+++ nuxt3/layouts/test.vue
@@ -1,27 +1,14 @@
-<template>{{ longestCommonPrefix(arr1) }}</template>
+<template>{{ twoSum(nums) }}</template>
 
 <script setup>
-const longestCommonPrefix = () => {
-  const arr1 = ['flower', 'flow', 'flight'];
-  let prefix = '';
-
-  // for (let i = 0; i < arr1[0].length; i++) {
-  //   const char = arr1[0][i];
-
-  //   for (let j = 1; j < arr1.length; j++) {
-  //     if (arr1[j][i] !== char) return prefix;
-  //   }
-  //   prefix = prefix + char;
-  // }
-  // return prefix;
-
-  for (let i = 0; i < arr1[0].length; i++) {
-    const char = arr1[0][i];
-
-    for (let j = 1; j < arr1.length; j++) {
-      if (arr1[j][i] !== char) return prefix;
+const longestCommonPrefix = (strs) => {
+  if (strs.length === 0) return '';
+  let prefix = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.substring(0, prefix.length - 1);
+      if (prefix === '') return '';
     }
-    prefix = prefix + char;
   }
   return prefix;
 };
