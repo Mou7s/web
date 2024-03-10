dbea2a84436b2dda1bcc076f5ff4bccc5957e305
mou7s <liuchuge7777777@gmail.com>
2024-03-11 02:16:12 +0800

    Update index.vue

--- nuxt3/pages/index.vue
+++ nuxt3/pages/index.vue
@@ -1,3 +1,13 @@
 <template>
-  <div class="grid place-content-center min-h-screen text-9xl">index</div>
+  <div class="grid place-content-center min-h-screen text-9xl">
+    {{ _comma(1234567) }}
+  </div>
 </template>
+
+<script setup>
+const fibonacci = (n) => {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+};
+</script>
