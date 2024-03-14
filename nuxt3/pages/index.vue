8558288201083e54d7aaeabee2bf3d8cac6bec2c
mou7s <liuchuge7777777@gmail.com>
2024-03-15 06:08:03 +0800

    Update index.vue

--- nuxt3/pages/index.vue
+++ nuxt3/pages/index.vue
@@ -1,13 +1,64 @@
 <template>
-  <div class="grid place-content-center min-h-screen text-9xl">
-    {{ _comma(1234567) }}
+  <div class="flex justify-center items-center">
+    <UCard>
+      <label for="eventListener">Event Listener:</label>
+      <UInput @keyup="handleKeyup" type="text" id="eventListener" />
+    </UCard>
+    <UCard>
+      <label for="eventListener2">Event Listener:</label>
+      <UInput @keyup="handleKeyup" type="text" id="eventListener2" />
+    </UCard>
   </div>
 </template>
-
 <script setup>
-const fibonacci = (n) => {
-  if (n === 0) return 0;
-  if (n === 1) return 1;
-  return fibonacci(n - 1) + fibonacci(n - 2);
+const handleKeyup = (event) => {
+  switch (event.key) {
+    case 'x':
+      button_x();
+      break;
+    case 'y':
+      button_y();
+      break;
+    case 'a':
+      button_a();
+      break;
+    case 'b':
+      button_b();
+      break;
+    default:
+      console.log(event.key);
+  }
+};
+
+const button_x = () => {
+  jump();
+};
+
+const button_y = () => {
+  fireGun();
+};
+
+const button_a = () => {
+  swapWeapon();
+};
+
+const button_b = () => {
+  lurch();
+};
+// Game functions
+const jump = () => {
+  console.log('Jump!');
+};
+
+const fireGun = () => {
+  console.log('Fire!');
+};
+
+const swapWeapon = () => {
+  console.log('Swap!');
+};
+
+const lurch = () => {
+  console.log('Lurch!');
 };
 </script>
