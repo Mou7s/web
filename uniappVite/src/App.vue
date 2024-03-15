798a08769ec65aa859ec82705d130a1b91bbe110
mou7s <liuchuge7777777@gmail.com>
2024-03-15 23:54:10 +0800

    #

---
+++ uniappVite/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { onMounted, onUnmounted } from 'vue';
+
+onMounted(() => {
+  console.log('App Show');
+});
+
+onUnmounted(() => {
+  console.log('App Hide');
+});
+</script>
+
+<style>
+/*每个页面公共css */
+</style>
