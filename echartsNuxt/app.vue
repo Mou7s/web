ae1727240361a2fa077e52d2314ecf95860f1632
mou7s <liuchuge7777777@gmail.com>
2024-03-17 05:27:24 +0800

    Update Echarts and Vue-Echarts dependencies

--- echartsNuxt/app.vue
+++ echartsNuxt/app.vue
@@ -4,9 +4,3 @@
   <NuxtLoadingIndicator />
   <NuxtLayout><NuxtPage></NuxtPage></NuxtLayout>
 </template>
-
-<style>
-.Transition {
-  @apply transition-colors duration-500 ease-in-out;
-}
-</style>
--- echartsNuxt/pages/index.vue
+++ echartsNuxt/pages/index.vue
@@ -1,7 +1,1 @@
-<template>
-  <UContainer>
-    <p class="text-center text-5xl hover:text-green-500 Transition font-serif m-8">
-      Nuxt Starter
-    </p>
-  </UContainer>
-</template>
+<template>123</template>
