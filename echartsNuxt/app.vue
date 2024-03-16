c820d9eb460bc6eb50348a6eadf625ebaa23c283
mou7s <liuchuge7777777@gmail.com>
2024-03-17 04:01:06 +0800

    Update code formatting and fix minor bugs

---
+++ echartsNuxt/app.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <NuxtLoadingIndicator />
+  <NuxtLayout><NuxtPage></NuxtPage></NuxtLayout>
+</template>
+
+<style>
+.Transition {
+  @apply transition-colors duration-500 ease-in-out;
+}
+</style>
