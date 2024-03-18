55ad15769a39637aa4563636080651d15f128753
mou7s <liuchuge7777777@gmail.com>
2024-03-18 10:30:32 +0800

    #

--- viteStarter/src/App.vue
+++ viteStarter/src/App.vue
@@ -1,25 +1,19 @@
 <template>
-  <div class="grid place-content-center min-h-screen">
-    <button @click="log">log</button>
-    <button @click="logDebounce">logDebounce</button>
+  <div
+    aria-label="container"
+    class="flex justify-center min-h-screen dark:bg-black dark:text-white"
+  >
+    <div>
+      <p class="text-3xl Transition font-serif hover:text-green-500">
+        viteStarter
+      </p>
+    </div>
   </div>
 </template>
 <script setup>
-const log = () => {
-  console.log(123);
-};
-
-const debounce = (fn, delay) => {
-  let timeoutId;
-  return function (...args) {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(() => {
-      fn(...args);
-    }, delay);
-  };
-};
-
-const logDebounce = debounce(log, 1000);
+for (let i = 1; i <= 5; i++) {
+  setTimeout(function timer() {
+    console.log(i);
+  }, i * 1000);
+}
 </script>
