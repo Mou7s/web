3e82206665d460f8ec0b7969f67319cc02ce0c57
mou7s <liuchuge7777777@gmail.com>
2024-03-20 01:53:00 +0800

    Add vue-confetti-explosion package and update styles in index.vue

--- uniappVite/src/pages/index.vue
+++ uniappVite/src/pages/index.vue
@@ -1,15 +1,21 @@
 <template>
-  <view class="grid place-content-center min-h-screen  gap-4">
-    <image class="h-48 w-48 mx-auto" src="../static/logo.png" aria-label="logo"></image>
-    <view class="flex justify-center" aria-label="text-area">
-      <p class="text-9xl font-serif Transition hover:text-green-500" aria-label="title">{{ title }}</p>
-    </view>
+  <view class="grid place-content-center">
+
+    <text class="text-5xl  font-serif hover:text-green-500 Transition">
+      Uniapp
+    </text>
+
 
   </view>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-
-const title = ref('Hello Uniapp');
+import { ref, computed } from 'vue';
 </script>
+
+
+<style>
+.Transition {
+  @apply transition-colors ease-in-out duration-500;
+}
+</style>
