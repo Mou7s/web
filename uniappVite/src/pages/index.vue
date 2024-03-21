a83d0bb2d34e00bdeff8acb0ad0c124281b73563
mou7s <liuchuge7777777@gmail.com>
2024-03-21 12:47:16 +0800

    Add jQuery dependency and update UI elements

--- uniappVite/src/pages/index.vue
+++ uniappVite/src/pages/index.vue
@@ -9,6 +9,16 @@
       Uniapp
     </div>
 
+    <div class="grid place-content-center gap-4 min-h-screen">
+      <p><abbr title="aberration"> Abbr</abbr></p>
+      <address><a>address:liuchuge7777777@gmail.com</a></address>
+      <del>del</del>
+      <ins>ins</ins>
+      <kbd>kbd</kbd>
+      <p><mark>mark</mark></p>
+      <p>meter<meter value="0.6657"></meter></p>
+      <p>progress <progress value="0.6657"></progress></p>
+    </div>
   </div>
 </template>
 
