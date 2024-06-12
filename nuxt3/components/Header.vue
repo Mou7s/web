ae48e5e60882d4174815ba3ee8fe88c847383ad6
mou7s <liuchuge7777777@gmail.com>
2024-06-12 16:21:21 +0800

    chore: update npm dependencies to latest versions

--- nuxt3/components/Header.vue
+++ nuxt3/components/Header.vue
@@ -2,14 +2,10 @@
   <nav>
     <ol class="flex justify-center flex-row gap-4 p-4 items-center">
       <li>
-        <NuxtLink to="/" class="Transition ">
-          index
-        </NuxtLink>
+        <NuxtLink to="/" class="Transition"> index </NuxtLink>
       </li>
       <li>
-        <NuxtLink to="html" class="Transition ">
-          html
-        </NuxtLink>
+        <NuxtLink to="html" class="Transition"> html </NuxtLink>
       </li>
     </ol>
   </nav>
