09b632a59b485670dfa00f4719a54a0fd85082d8
mou7s <mou7s@icloud.com>
2026-02-01 18:47:02 +0800

    refresh

--- test.js
+++ test.js
@@ -1,14 +1,1 @@
-const nextGreatestLetter = (letters, target) => {
-  let left = 0;
-  let right = letters.length;
-
-  while (left < right) {
-    const m = (left + right) >> 1;
-    if (letters[m] <= target) {
-      left = m + 1;
-    } else {
-      right = m;
-    }
-  }
-  return letters[left] || letters[0];
-};
+console.log("hello world!")
